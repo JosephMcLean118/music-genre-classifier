8173c6a88b673042bb2de3e6f42410120cfb4194
JosephMclean118 <josephjmclean392@gmail.com>
2025-07-13 18:32:20 +0100

    Added feature that allows user to play and pause audio track

--- genre_predictor.py
+++ genre_predictor.py
@@ -1,5 +1,13 @@
 import customtkinter
 from customtkinter import filedialog as fd
+import numpy as np
+import librosa
+import librosa.display
+import IPython.display as ipd
+from glob import glob
+import pygame
+
+
 
 #Use custom tkinter for a more modern look
 customtkinter.set_appearance_mode("dark")
@@ -11,20 +19,47 @@ root = customtkinter.CTk()
 root.geometry("500x350")
 root.title("Music Genre Predicter")
 
+
+#Functions for all buttons in the GUI - UPLOAD, PLAY, PAUSE/RESUME
+
 def file_upload():
-    file_path = fd.askopenfilename()
+    global file_path 
+    file_path = fd.askopenfilename(filetypes=[("Audio Files", "*.mp3")])
     if file_path:
         #check file path
         print(f"File selected: {file_path}")
 
-frame = customtkinter.CTkFrame(master=root)
-frame.pack(pady=20, padx=20, fill="both", expand=True)
+def play_song():
+    pygame.mixer.init()
+    pygame.mixer.music.load(file_path)
+    pygame.mixer.music.play()
+
+pause = False
+def pause_song():
+    global pause
+    if pause:
+        pygame.mixer.music.unpause()
+        pause = False
+    else:
+        pygame.mixer.music.pause()
+        pause = True
+
+top_frame = customtkinter.CTkFrame(master=root)
+top_frame.pack(pady=20, padx=20, fill="both", expand=True)
 
 
-label = customtkinter.CTkLabel(master=frame, text="MUSIC GENRE PREDICTER", font=customtkinter.CTkFont(family='Roboto', size=37))
+label = customtkinter.CTkLabel(master=top_frame, text="MUSIC GENRE PREDICTER", font=customtkinter.CTkFont(family='Roboto', size=37))
 label.pack(pady=12, padx=10)
 
-file_button = customtkinter.CTkButton(master=frame, text="Upload File", command=file_upload)
+file_button = customtkinter.CTkButton(master=top_frame, text="Upload File", command=file_upload)
 file_button.pack(pady=12, padx=10)
+
+
+
+play_button = customtkinter.CTkButton(master = top_frame, text="Play", command = play_song)
+play_button.pack(pady=12, padx=10)
+
+pause_button = customtkinter.CTkButton(master = top_frame, text="Pause/Resume", command = pause_song)
+pause_button.pack(pady=12, padx=10)
 
 root.mainloop()
